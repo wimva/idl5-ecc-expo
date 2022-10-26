8233970aa82339e0303e5f4d5edb2342702e7a0c
Wim Van Aerschot <wim@vaw.be>
2022-10-26 21:28:43 +0200

    added custom font

--- App.js
+++ App.js
@@ -2,25 +2,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/Home';
 import DetailsScreen from './screens/Details';
-import AppLoading from 'expo-app-loading';
-
-import {
-  useFonts,
-  Inter_900Black,
-} from '@expo-google-fonts/inter';
+import { useFonts } from 'expo-font';
+import { useEffect, useCallback } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Inter_900Black,
+  const [fontsLoaded] = useFonts({
+    'Poppins-Black': require('./assets/fonts/Poppins-Black.ttf'),
   });
 
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <NavigationContainer>
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView} style={{flex:1}}>
+      <NavigationContainer >
         <Tab.Navigator screenOptions={{
           headerShown: false
         }}>
@@ -28,6 +41,6 @@ export default function App() {
           <Tab.Screen name="Details" component={DetailsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
-    );
-  }
+    </View>
+  );
 }
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -9,6 +9,6 @@ export const styles = StyleSheet.create({
   },
   text: {
     color: 'red',
-    fontFamily: 'Inter_900Black'
+    fontFamily: 'Poppins-Black'
   }
 });
